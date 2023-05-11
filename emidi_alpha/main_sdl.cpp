38ead74e814fc5404f7ae658b114c66ae6f714a9
Wohlstand <admin@wohlnet.ru>
2023-05-11 09:32:27 +0300

    main_sdl.cpp: Added signal handling and turned code style to original

--- emidi_alpha/main_sdl.cpp
+++ emidi_alpha/main_sdl.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <signal.h>
 #include "CSMFPlay.hpp"
 
 enum { nch = 2 };
@@ -11,10 +12,15 @@ static int buf[buffer_size * nch];
 /* variable declarations */
 static Uint32 is_playing = 0; /* remaining length of the sample we have to play */
 
+static void sighook(int)
+{
+  is_playing = 0;
+}
+
 struct MidPlayInstance
 {
-    dsa::CSMFPlay *p;
-    int buf[buffer_size * nch];
+  dsa::CSMFPlay *p;
+  int buf[buffer_size * nch];
 };
 
 /*
@@ -25,81 +31,78 @@ struct MidPlayInstance
 */
 void my_audio_callback(void *midi_player, Uint8 *stream, int len)
 {
-    MidPlayInstance *mp = (MidPlayInstance*)midi_player;
-    int i = 0;
-    int count = len / sizeof(short);
-    short* dest = (short*)stream;
-    DWORD got = mp->p->Render(buf, (count >> 1));
-    if(got == 0)
-        is_playing = 0;
-    for(i = 0; i < count; i += 2)
-    {
-        *(dest++) = buf[i];
-        *(dest++) = buf[i + 1];
-    }
+  MidPlayInstance *mp = (MidPlayInstance*)midi_player;
+  int i = 0;
+  int count = len / sizeof(short);
+  short* dest = (short*)stream;
+  DWORD got = mp->p->Render(buf, (count >> 1));
+  if(got == 0)
+    is_playing = 0;
+  for(i = 0; i < count; i += 2) {
+    *(dest++) = buf[i];
+    *(dest++) = buf[i + 1];
+  }
 }
-
-static volatile int done = 0 ;
 
 int main(int argc, char *argv[])
 {
-    static SDL_AudioSpec            spec; /* the specs of our piece of music */
-    int b  = 0 ,c , song = 0 ;
-    MidPlayInstance mp;
-    mp.p = new dsa::CSMFPlay(44100, 8);
+  static SDL_AudioSpec            spec; /* the specs of our piece of music */
+  int b  = 0 ,c , song = 0 ;
+  MidPlayInstance mp;
+  mp.p = new dsa::CSMFPlay(44100, 8);
 
-    if(argc<=1) {
-        printf("%s filename.mid\n", argv[0]);
-        return 0;
-    }
+  if(argc<=1) {
+    printf("%s filename.mid\n", argv[0]);
+    return 0;
+  }
 
-    if(!mp.p->Open(argv[1])) {
-        printf("File open error!\n");
-        return 1;
-    }
+  if(!mp.p->Open(argv[1])) {
+      printf("File open error!\n");
+      return 1;
+  }
 
-    mp.p->Start();
+  signal(SIGINT, &sighook);
+  signal(SIGTERM, &sighook);
+
+  mp.p->Start();
 
 
-    /* Initialize SDL.*/
-    if(SDL_Init(SDL_INIT_AUDIO) < 0)
-        return 1;
+  /* Initialize SDL.*/
+  if(SDL_Init(SDL_INIT_AUDIO) < 0)
+    return 1;
 
-    memset(&spec, 0, sizeof(SDL_AudioSpec));
-    spec.freq = 44100;
-    spec.format = AUDIO_S16SYS;
-    spec.channels = 2;
-    spec.samples = buffer_size;
+  memset(&spec, 0, sizeof(SDL_AudioSpec));
+  spec.freq = 44100;
+  spec.format = AUDIO_S16SYS;
+  spec.channels = 2;
+  spec.samples = buffer_size;
 
-    /* set the callback function */
-    spec.callback = my_audio_callback;
-    /* set ADLMIDI's descriptor as userdata to use it for sound generation */
-    spec.userdata = &mp;
+  /* set the callback function */
+  spec.callback = my_audio_callback;
+  /* set ADLMIDI's descriptor as userdata to use it for sound generation */
+  spec.userdata = &mp;
 
-    /* Open the audio device */
-    if (SDL_OpenAudio(&spec, NULL) < 0)
-    {
-        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
-        return 1;
-    }
+  /* Open the audio device */
+  if (SDL_OpenAudio(&spec, NULL) < 0) {
+    fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+    return 1;
+  }
 
-    is_playing = 1;
-    /* Start playing */
-    SDL_PauseAudio(0);
+  is_playing = 1;
+  /* Start playing */
+  SDL_PauseAudio(0);
 
-    printf("Playing... Hit Ctrl+C to quit!\n");
+  printf("Playing... Hit Ctrl+C to quit!\n");
 
-    /* wait until we're don't playing */
-    while(is_playing)
-    {
-        SDL_Delay(100);
-    }
+  /* wait until we're don't playing */
+  while(is_playing) {
+    SDL_Delay(100);
+  }
 
-    /* shut everything down */
-    SDL_CloseAudio();
+  /* shut everything down */
+  SDL_CloseAudio();
 
-    delete mp.p;
+  delete mp.p;
 
-    return 0 ;
+  return 0 ;
 }
-
